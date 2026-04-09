dae5f47eb918ba1bcc0ba01e9478858b2906e426
Ananya S <ananya.subramonian22@spgs.org>
2026-04-09 10:39:13 +0100

    to team name and number

--- web.py
+++ web.py
@@ -261,7 +261,7 @@ def login():
                         session['user'] = u
                         return redirect('/')
         return render_template_string(
-            base_style + '<div class="container" style="max-width:400px"><div class="card"><h2>LOGIN</h2><form method="POST"><input name="username" placeholder="USER"><input type="password" name="password" placeholder="PASS"><button>LOGIN</button></form><a href="/register" style="font-size:13px; color:var(--text-muted);">Create Account</a><h4>INVALID LOGIN - have you created an account yet?</h4></div></div>')
+            base_style + '<div class="container" style="max-width:400px"><div class="card"><h2>LOGIN</h2><form method="POST"><input name="username" placeholder="Team Name"><input name="password" placeholder="Team Number"><button>LOGIN</button></form><a href="/register" style="font-size:13px; color:var(--text-muted);">Create Account</a><h4>INVALID LOGIN - have you created an account yet?</h4></div></div>')
     return render_template_string(
         base_style + '<div class="container" style="max-width:400px"><div class="card"><h2>LOGIN</h2><form method="POST"><input name="username" placeholder="USER"><input type="password" name="password" placeholder="PASS"><button>LOGIN</button></form><a href="/register" style="font-size:13px; color:var(--text-muted);">Create Account</a></div></div>')
 
